a0f592dade95895f3bd7f60d73a43b4cee1f2722
Fantamaz781 <83710497+Fantamaz781@users.noreply.github.com>
2021-05-05 17:35:31 +0400

    Create teadorsalvador.sh

---
+++ teadorsalvador.sh
@@ -0,0 +1,122 @@
+apt update -y
+apt upgrade -y
+pkg install termux-auth -y
+pkg install proot-distro -y
+pkg install ghostscript -y
+pkg install helm -y
+pkg install vlang -y
+pkg install python -y
+pkg install o-editor -y
+pkg install emacs -y
+pkg install hfsutils -y
+pkg install tor -y
+pkg install imagemagick -y
+pkg install proxychains-ng -y
+pkg install e2fsproqs -y
+pkg install python2 -y
+pkg install mathomatic -y
+pkg install lnd -y
+pkg install python3 -y
+pkg install golang -y
+pkg install pip -y
+pkg install pip2 -y
+pkg install php -y
+pkg install crunch -y
+pkg install curl -y
+pkg install wget -y
+pkg install nano -y
+pkg install irssi -y
+pkg install git -y
+pkg install micro -y
+pkg install fish -y
+pkg install tmux -y
+pkg install proot -y
+pkg install python-tkinder -y
+pkg install termux-tools -y
+pkg install runit -y
+pkg install vim -y
+pkg install kona -y
+pkg install sc -y
+pkg install openssh -y
+pkg install grep -y
+pkg install ncurses-utils -y
+pkg install unstable-repo -y
+pkg install nmh -y
+pkg install xmlstarlet -y
+pkg install ruby -y
+pkg install nodejs -y
+pkg install cocofire -y
+pkg install cmatrix -y
+pkg install mtools -y
+pkg install x2x -y
+pkg install hoedown -y
+pkg install zeronet -y
+pkg install rcs -y
+pkg install lf -y
+pkg upgrade termux-auth -y
+pkg upgrade proot-distro -y
+pkg upgrade ghostscript -y
+pkg upgrade helm -y
+pkg upgrade vlang -y
+pkg upgrade python -y
+pkg upgrade o-editor -y
+pkg upgrade emacs -y
+pkg upgrade hfsutils -y
+pkg upgrade tor -y
+pkg upgrade imagemagick -y
+pkg upgrade proxychains-ng -y
+pkg upgrade lnd -y
+pkg upgrade e2fsproqs -y
+pkg upgrade python2 -y
+pkg upgrade mathomatic -y
+pkg upgrade python3 -y
+pkg upgrade golang -y
+pkg upgrade pip -y
+pkg upgrade pip2 -y
+pkg upgrade php -y
+pkg upgrade crunch -y
+pkg upgrade curl -y
+pkg upgrade wget -y
+pkg upgrade nano -y
+pkg upgrade irssi -y
+pkg upgrade git -y
+pkg upgrade micro -y
+pkg upgrade fish -y
+pkg upgrade tmux -y
+pkg upgrade proot -y
+pkg upgrade python-tkinder -y
+pkg upgrade termux-tools -y
+pkg upgrade runit -y
+pkg upgrade vim -y
+pkg upgrade kona -y
+pkg upgrade sc -y
+pkg upgrade openssh -y
+pkg upgrade grep -y
+pkg upgrade ncurses-utils -y
+pkg upgrade unstable-repo -y
+pkg upgrade nmh -y
+pkg upgrade xmlstarlet -y
+pkg upgrade ruby -y
+pkg upgrade nodejs -y
+pkg upgrade cocofire -y
+pkg upgrade cmatrix -y
+pkg upgrade mtools -y
+pkg upgrade x2x -y
+pkg upgrade hoedown -y
+pkg upgrade zeronet -y
+pkg upgrade rcs -y
+pkg upgrade lf -y
+pip install --upgrade pip
+pip install colorama
+pip install bs4
+pip install pyngrok
+pip install pgrep
+pip install requests
+pip install pyaes
+pip install mechanize
+pip install rsa
+pip install clint
+pip install wordlist
+pip install async_generator
+pip install telethon
+termux-open-url https://www.instagram.com/teadorhacking
